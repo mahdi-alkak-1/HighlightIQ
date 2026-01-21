8f8e45b40e5d332b2885585a5e521235c6d711cd
mahdi-alkak1-1 <mahdialkak1@gmail.com>
2026-01-21 02:23:50 +0200

    Pagination for clips candidates Implemented

--- highlight-client/src/data/clipStudioData.ts
+++ highlight-client/src/data/clipStudioData.ts
@@ -9,3 +9,5 @@ export const hashtagOptions = ["#clutch", "#ace", "#shorts"];
 export const timelinePoints = [12, 24, 10, 28, 16, 32, 18, 40, 22, 36, 20, 30];
 
 export const maxClipDurationSeconds = 59;
+
+export const candidatePageSize = 5;
